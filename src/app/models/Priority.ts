9ca72c3cc8f0edd5b6b48f7f4fefca55da26197e
= <=>
2024-12-01 14:54:00 +0530

    priority model update

--- src/app/models/Priority.ts
+++ src/app/models/Priority.ts
@@ -1,6 +1,7 @@
 export class Priority {
   private priority: number;
   private index: string
+  private id: string;
   private color: string;
   private task: string;
   private date: Date;
@@ -8,6 +9,7 @@ export class Priority {
   constructor(priority: number, index: string, color: string, task: string, date: Date) {
     this.priority = priority;
     this.index = index;
+    this.id = this.priority + this.index;
     this.color = color;
     this.task = task;
     this.date = date;
