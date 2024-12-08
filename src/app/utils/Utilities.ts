e4422dc680decebd2ab54b3f1c14c1a8044faf97
= <=>
2024-12-08 16:02:41 +0530

    getting current date

--- src/app/utils/Utilities.ts
+++ src/app/utils/Utilities.ts
@@ -4,12 +4,15 @@ import {Injectable} from '@angular/core';
   providedIn: 'root'
 })
 export class Utilities {
-  public getCurrentDateFormatted(): string {
-    const today = new Date();
-    const month = (today.getMonth() + 1).toString().padStart(2, "0"); // getMonth() is zero-based
-    const day = today.getDate().toString().padStart(2, "0");
-    const year = today.getFullYear();
+  public getFormattedDate(date: Date): string {
+    const month = (date.getMonth() + 1).toString().padStart(2, "0"); // getMonth() is zero-based
+    const day = date.getDate().toString().padStart(2, "0");
+    const year = date.getFullYear();
 
     return `${month}/${day}/${year}`;
+  }
+
+  public getCurrentDate() {
+    return this.getFormattedDate(new Date());
   }
 }
